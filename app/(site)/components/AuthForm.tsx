3e0358ba15dd3ea094915ac25b0de2edb2c190b3
pabel183 <jhpabel183@gmail.com>
2024-01-30 18:59:55 +0600

    Sign in complete using credentials

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,8 @@ import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type variant = "Login" | "Register";
 
@@ -39,10 +41,25 @@ const Authform = () => {
         setIsLoading(true);
 
         if (variant === "Register") {
-            axios.post("/api/register",data);
+            axios.post("/api/register",data)
+            .catch(()=>toast.error("Something went wrong!"))
+            .finally(()=>setIsLoading(false));
         }
         if (variant === "Login") {
-            //NextAuth SignIn
+            signIn("credentials",{
+                ...data,
+                redirect:false
+            })
+            .then((callback)=>{
+                if(callback?.error){
+                    toast.error("Invalid credentials");
+                }
+
+                if(callback?.ok && !callback?.error){
+                    toast.success("Logged in!");
+                }
+            })
+            .finally(()=>setIsLoading(false));
         }
     }
 
