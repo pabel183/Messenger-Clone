fc81c7c6f96dcc0ef5f171e17b5f957967dd6624
pabel183 <jhpabel183@gmail.com>
2024-02-12 19:36:49 +0600

    avater is added to the bottom left corner

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
+import { User } from "@prisma/client";
+
+import useRoutes from "@/app/hooks/useRoutes";
+
+import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar=()=>{
+interface DesktopSidebarProps{
+    currentUser: User
+}
+const DesktopSidebar: React.FC<DesktopSidebarProps>=({
+    currentUser
+})=>{
     const routes=useRoutes();
     const [isOpen, setIsOpen]=useState();
 
+    console.log({currentUser});
     return (
         <div
         className="
@@ -52,6 +62,22 @@ const DesktopSidebar=()=>{
                         )
                     )}
                 </ul>
+            </nav>
+            <nav
+            className="
+            mt-4
+            flex
+            flex-col
+            justify-between
+            items-center
+            ">
+                <div className="
+                cursor-pointer
+                hover:opacity-75
+                transition
+                ">
+                    <Avatar user={currentUser}/>
+                </div>
             </nav>
         </div>
     )
