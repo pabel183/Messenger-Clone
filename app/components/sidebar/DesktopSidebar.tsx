75da8223fcd494c11eb8afa814664b1501a5e889
pabel183 <jhpabel183@gmail.com>
2024-02-13 10:48:30 +0600

    userList of people page is done

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -17,7 +17,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps>=({
     const routes=useRoutes();
     const [isOpen, setIsOpen]=useState();
 
-    console.log({currentUser});
+    // console.log({currentUser});
     return (
         <div
         className="
