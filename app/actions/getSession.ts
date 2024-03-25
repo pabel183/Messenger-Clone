1e259eaafef482ea86397cb6cfe050ab6fce5e0d
pabel183 <jhpabel183@gmail.com>
2024-03-25 15:03:37 +0600

    fix: rename authOptions directory file to authOptions.js

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import authOptions from "../libs/auth";
+import authOptions from "../libs/authOptions";
 
 
 export default async function getSession() {
